04236b42a06e6d800db8517ec000ce2b2c4d49e9
marixane <prof.marwane@gmail.com>
2026-07-07 20:13:37 +0100

    Reduce progress bar to 140 pixels

--- src/cahier-progress-bar-class.js
+++ src/cahier-progress-bar-class.js
@@ -9,19 +9,19 @@ const tagHomeworkProgressBars = () => {
     if (progressWrap) {
       progressWrap.classList.add('cahier-progress-wrap');
       progressWrap.style.setProperty('display', 'grid', 'important');
-      progressWrap.style.setProperty('grid-template-columns', '220px 46px', 'important');
-      progressWrap.style.setProperty('width', '276px', 'important');
-      progressWrap.style.setProperty('min-width', '276px', 'important');
-      progressWrap.style.setProperty('max-width', '276px', 'important');
+      progressWrap.style.setProperty('grid-template-columns', '140px 46px', 'important');
+      progressWrap.style.setProperty('width', '196px', 'important');
+      progressWrap.style.setProperty('min-width', '196px', 'important');
+      progressWrap.style.setProperty('max-width', '196px', 'important');
       progressWrap.style.setProperty('justify-self', 'center', 'important');
       progressWrap.style.setProperty('gap', '10px', 'important');
     }
 
     if (progressBar) {
       progressBar.classList.add('cahier-progress-bar');
-      progressBar.style.setProperty('width', '220px', 'important');
-      progressBar.style.setProperty('min-width', '220px', 'important');
-      progressBar.style.setProperty('max-width', '220px', 'important');
+      progressBar.style.setProperty('width', '140px', 'important');
+      progressBar.style.setProperty('min-width', '140px', 'important');
+      progressBar.style.setProperty('max-width', '140px', 'important');
     }
   });
 };
